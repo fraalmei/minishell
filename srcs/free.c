c13c7f79366f00025e4a02e408aa7b75b05866ab
Francisco José Almeida Orcha <104362305+fraalmei@users.noreply.github.com>
2023-05-12 12:05:18 +0200

    Add files via upload

--- srcs/free.c
+++ srcs/free.c
@@ -6,12 +6,13 @@
 /*   By: fraalmei <fraalmei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 15:02:58 by fraalmei          #+#    #+#             */
-/*   Updated: 2023/05/10 12:21:59 by fraalmei         ###   ########.fr       */
+/*   Updated: 2023/05/12 11:16:12 by fraalmei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
+	// free the t_promt struct (or structs) generated from the buffer
 int	free_prompt(t_prompt *prom)
 {
 	while (prom)
@@ -24,6 +25,26 @@ int	free_prompt(t_prompt *prom)
 			free (prom->options);
 		free (prom);
 		prom = prom->next;
+	}
+	return (0);
+}
+
+int	free_env(t_env *env)
+{
+	t_env_var	*swap;
+
+	if (env)
+	{
+		free_str (env->env);
+		while (env->frst)
+		{
+			swap = env->frst;
+			free (swap->var);
+			free (swap->value);
+			env->frst = swap->next;
+			free (swap);
+		}
+		free (env);
 	}
 	return (0);
 }
