b6f84ad798867cb37d357eca5dfd0b3ce0daaac3
Francisco José Almeida Orcha <104362305+fraalmei@users.noreply.github.com>
2023-05-13 13:15:46 +0200

    Add files via upload

---
+++ include/basics.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   basics.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fraalmei <fraalmei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/27 15:15:07 by fraalmei          #+#    #+#             */
+/*   Updated: 2023/03/29 12:24:41 by fraalmei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BASICS_H
+# define BASICS_H
+
+# include <minishell.h>
+
+// ------ DEFINES ------
+# define MSJ_ERROR "Error de sintaxis\n"
+
+// -------- ENUMS --------
+/* Boolean */
+enum e_bool
+{
+	TRUE = 1,
+	FALSE = 0
+}		bool;
+
+#endif
