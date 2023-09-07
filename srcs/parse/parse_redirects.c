14963f3ebffbfcd51a46c8738b3aff4aba197670
fraalmei <fraalmei@student.42madrid.com>
2023-09-07 10:32:58 +0200

    ver 7.9.23 modificado recogida de argumentos output

--- srcs/parse/parse_redirects.c
+++ srcs/parse/parse_redirects.c
@@ -6,7 +6,7 @@
 /*   By: fraalmei <fraalmei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 17:01:31 by fraalmei          #+#    #+#             */
-/*   Updated: 2023/09/04 16:30:53 by fraalmei         ###   ########.fr       */
+/*   Updated: 2023/09/07 10:31:35 by fraalmei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,29 @@ static char	**redir_join(char *buffer, int *i, char **redir)
 	*i -= 1;
 	return (redir);
 }
+static char	**output_join(char *buffer, int *i, char **redir)
+{
+	char	*swap;
+
+	swap = NULL;
+	swap = ft_strjoin_allfree (swap, ft_strndup(&buffer[*i], is_redir(&buffer[*i])));
+	*i += is_redir(&buffer[*i]);
+	while (buffer[*i] == ' ')
+		*i += 1;
+	if (!buffer[*i] && is_redirecction(&buffer[*i]) != 0)
+	{
+		redir = str_strjoin_freeall(redir, swap);
+		*i -= 1;
+		return (redir);
+	}
+	swap = ft_strjoin_allfree(swap, read_word(buffer, i));
+	*i += 1;
+	while (buffer[*i] && is_redirecction(&buffer[*i]) == 0)
+		*i += 1;
+	redir = str_strjoin_freeall(redir, swap);
+	*i -= 1;
+	return (redir);
+}
 
 int	get_redir(char *buffer, int *i, t_prompt *swap)
 {
@@ -34,9 +57,7 @@ int	get_redir(char *buffer, int *i, t_prompt *swap)
 		swap->input_redirect = redir_join(buffer, i, swap->input_redirect);
 	else if (buffer[*i] == '<' && buffer[*i + 1] == '<')
 		swap->here_doc = redir_join(buffer, i, swap->here_doc);
-	else if (buffer[*i] == '>' && buffer[*i + 1] != '>')
-		swap->output_redirect = redir_join(buffer, i, swap->output_redirect);
-	else if (buffer[*i] == '>' && buffer[*i + 1] == '>')
-		swap->append_redirect = redir_join(buffer, i, swap->append_redirect);
+	else if (buffer[*i] == '>')
+		swap->output_redirect = output_join(buffer, i, swap->output_redirect);
 	return (0);
 }
