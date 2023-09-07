5cdd09cdde880d0f200177d6684e977b59df28ab
fraalmei <fraalmei@student.42madrid.com>
2023-09-07 10:47:59 +0200

    ver 7.9.23 actualizado echo

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: fraalmei <fraalmei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 09:36:41 by fraalmei          #+#    #+#             */
-/*   Updated: 2023/09/02 17:11:35 by fraalmei         ###   ########.fr       */
+/*   Updated: 2023/09/07 10:45:57 by fraalmei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,21 @@ int	echo(t_prompt *prom)
 	char	**swap;
 	int		i;
 
-	i = 0;
+	i = 1;
 	swap = prom->arguments;
 	if (prom->n_options > 0)
 		swap++;
+	swap++;
 	while (i < prom->n_arguments)
 	{
 		if (*swap)
-			printf("%s ", *swap);
+			printf("%s", *swap);
 		swap++;
 		i++;
+		if (i < prom->n_arguments)
+			printf(" ");
 	}
-	if (prom->n_options > 0 && ft_strcmp(prom->arguments[0], "n") == 0)
+	if (prom->n_options > 0 && ft_strcmp(prom->arguments[1], "n") == 0)
 		return (0);
 	else
 		printf ("\n");
