d2bb7a3cd4db1425120c945a5d9edf41cb70cf62
Carlos Gonzalez villalba <cagonzal@student.42madrid.com>
2023-10-02 13:41:21 +0200

    ver 02/10/2024 - 13:40 Update for unset PATH work

--- libft/ft_error.c
+++ libft/ft_error.c
@@ -6,7 +6,7 @@
 /*   By: cagonzal <cagonzal@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 10:04:38 by vpujalte          #+#    #+#             */
-/*   Updated: 2023/09/15 09:38:32 by cagonzal         ###   ########.fr       */
+/*   Updated: 2023/10/02 13:37:18 by cagonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,21 @@ void	ft_error(int error, char *argument)
 	{
 		if (error == -1)
 		{
-			ft_putstr_fd("pipex: Command not found: ", STDERR);
+			ft_putstr_fd("minishell: ", STDERR);
 			ft_putstr_fd(argument, STDERR);
+			ft_putstr_fd(": No such file or directory", STDERR);
 			ft_putstr_fd("\n", STDERR);
 		}
 		if (error == -2)
-			ft_putstr_fd("pipex: Invalid number of arguments\n", STDERR);
+			ft_putstr_fd("minishell: Invalid number of arguments\n", STDERR);
 		if (error == -3)
-			ft_putstr_fd("pipex: Error at dup2\n", STDERR);
-		exit(0);
+			ft_putstr_fd("minishell: Error at dup2\n", STDERR);
 	}
 }
 
 /*
 {
-	ft_putstr_fd("pipex: ", STDERR);
+	ft_putstr_fd("minishell: ", STDERR);
 	ft_putstr_fd(strerror(error), STDERR);
 	ft_putstr_fd(": ", STDERR);
 	ft_putstr_fd(argument, STDERR);
