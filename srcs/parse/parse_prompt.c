cb177cb746193d0dc4274e4fca25afea43f64f14
fraalmei <fraalmei@student.42madrid.com>
2023-11-02 13:59:14 +0100

    ver 1.11.23 GOLD minishell 1.0.3

--- srcs/parse/parse_prompt.c
+++ srcs/parse/parse_prompt.c
@@ -6,7 +6,7 @@
 /*   By: fraalmei <fraalmei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 15:40:44 by fraalmei          #+#    #+#             */
-/*   Updated: 2023/11/01 17:23:33 by fraalmei         ###   ########.fr       */
+/*   Updated: 2023/11/02 13:58:24 by fraalmei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,7 +69,6 @@ static t_prompt	*add_prom(char *buff, t_prompt **prom, t_prompt *swap, int *i)
 	swap = new_prompt_struct();
 	swap->sep0 = last_prom(*prom)->sep1;
 	swap->pos_p += last_prom(*prom)->pos_p;
-	i[0]++;
 	return (swap);
 }
 
